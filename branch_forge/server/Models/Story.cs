5f5215da9a67ba9b58a8ca26d6eca50e4fae324c
Roman Wernsing <wernsingroman@gmail.com>
2025-05-14 16:08:52 -0600

    created story model : server

--- branch_forge/server/Models/Story.cs
+++ branch_forge/server/Models/Story.cs
@@ -3,4 +3,8 @@ namespace branch_forge.Models;
 public class Story : RepoItem<int>
 {
   public string Description { get; set; }
+  public string Title { get; set; }
+  public string CoverImg { get; set; }
+  public int LikeCount { get; set; }
+  public string AuthorId { get; set; }
 }
